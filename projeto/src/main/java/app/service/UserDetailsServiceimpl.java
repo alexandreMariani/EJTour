eccb6970fffee06c1a5ede645bfe37da08795965
jean <jeanvictor20032@gmail.com>
2025-05-09 16:32:44 -0300

    feito jwt

--- projeto/src/main/java/app/service/UserDetailsServiceimpl.java
+++ projeto/src/main/java/app/service/UserDetailsServiceimpl.java
@@ -5,11 +5,12 @@ import app.entity.UserAuthenticated;
 import app.repository.UserRepository;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailsServiceimpl implements org.springframework.security.core.userdetails.UserDetailsService {
+public class UserDetailsServiceimpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
